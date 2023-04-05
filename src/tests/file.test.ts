8f1d2c0dbcc3b3f331e1a6fae913a0c64ba298b5
Angelo Munhoz <angelomca09@gmail.com>
2023-04-04 21:05:14 -0300

    Fixing readFile Test

--- src/tests/file.test.ts
+++ src/tests/file.test.ts
@@ -3,9 +3,10 @@ import fs from "fs";
 
 describe("Files", () => {
   const buffer = fs.readFileSync("sales.txt");
-  test("Can read files", () => {
-    expect(fileService.readFile(buffer)).not.toBeUndefined();
-    expect(fileService.readFile(buffer)).toEqual(Buffer.from(buffer.toString(), "utf8").toString())
+  test("Can read files", async () => {
+    const file = await fileService.readFile(buffer)
+    expect(file).not.toBeUndefined();
+    expect(file).toEqual(Buffer.from(buffer.toString(), "utf8").toString())
   });
   test("Can parse files", () => {
     expect(fileService.parseFile(buffer)).not.toBeUndefined()

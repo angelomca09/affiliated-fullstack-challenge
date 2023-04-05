93af4551ef4029cce7b651b4bcd919fb3925038b
Angelo Munhoz <angelomca09@gmail.com>
2023-04-05 10:08:38 -0300

    Adding Routes for Sales

--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -3,6 +3,8 @@ import fileController from "../controllers/file.controller";
 
 const route = express.Router();
 
+route.get("/sales", fileController.getSales)
+route.get("/sale/:sale_id", fileController.getSale)
 route.post("/fileupload", fileController.readFile)
 
 export default route;

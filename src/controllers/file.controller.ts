e1b008c4b46887ef647549c6f9a66bb183e93c1a
Angelo Munhoz <angelomca09@gmail.com>
2023-04-05 23:06:12 -0300

    Changing file name from Formdata

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -3,7 +3,7 @@ import fileService from "../services/file.service";
 
 async function readFile(req: express.Request, res: express.Response, next: express.NextFunction) {
   try {
-    const file = (req.files?.teste as any)
+    const file = (req.files?.txt as any)
     const buffer: Buffer = file.data;
     res.send(await fileService.parseFile(buffer));
   } catch (err) {

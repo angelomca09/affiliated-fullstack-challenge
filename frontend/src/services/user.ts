acba29a0cc65c848ce61ddfa400eaaca59f218a6
Angelo Munhoz <angelomca09@gmail.com>
2023-04-06 02:52:22 -0300

    Frontend User Services

---
+++ frontend/src/services/user.ts
@@ -0,0 +1,43 @@
+import ILogin from "../interfaces/login.interface";
+
+function getUser(): ILogin | null {
+  const user = sessionStorage.getItem("user");
+  if (!user) return null;
+  return JSON.parse(user);
+}
+
+function setUser(user: ILogin): void {
+  sessionStorage.setItem("user", JSON.stringify(user));
+}
+
+function removeUser(): void {
+  sessionStorage.removeItem("user");
+}
+
+function getToken(): string | null {
+  const token = sessionStorage.getItem("token");
+  if (!token) return null;
+  return JSON.parse(token);
+}
+
+function setToken(token: string): void {
+  sessionStorage.setItem("token", JSON.stringify(token));
+}
+
+function removeToken(): void {
+  sessionStorage.removeItem("token");
+}
+
+function clearStorage() {
+  sessionStorage.clear();
+}
+
+export {
+  getUser,
+  setUser,
+  removeUser,
+  getToken,
+  setToken,
+  removeToken,
+  clearStorage
+}
